d616e37a1fb10862b217dc337dde5987c62e268b
Fatih BAYTAR <matanist@hotmail.com>
2020-06-06 21:39:46 +0300

    Add project files.

---
+++ BilmemYol.WebAPI/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BilmemYol.Data.Models;
+using BilmemYol.WebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BilmemYol.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly BilmemYolContext _context;
+        public ProductController(BilmemYolContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        [Route("GetProducts")]
+        public ApiResponse GetProducts()
+        {
+            try
+            {
+                var products = _context.Product.ToList();
+                return new ApiResponse { Code = 200, Message = "Success", Set = products };
+            }
+            catch (Exception exp)
+            {
+                return new ApiResponse { Code = 500, Message = "İç hata oluştu", Set = exp.StackTrace };
+            }
+
+        }
+        [HttpGet("GetProductNameById")]
+        public ApiResponse GetProductNameById(int? id)
+        {
+            return new ApiResponse { Code = 200, Message = "Success", Set = "Araba" };
+        }
+    }
+}
---
+++ BilmemYol.WebAPI/Models/ApiResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BilmemYol.WebAPI.Models
+{
+    public class ApiResponse
+    {
+        public int Code { get; set; }
+        public string Message { get; set; }
+        public object Set { get; set; }
+    }
+}
